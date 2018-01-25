f5e0e56982364a47b345652ae930b9bf04ff791b
User104 <User104@GUY-PC>
2018-01-25 10:06:25 +0200

    pie-chart

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import axios from "axios";
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Route, browserHistory, Redirect } from 'react-router';
+import { Chart, Axis, Series, Tooltip, Cursor, Line } from 'react-charts';
+import Pie3D from 'react-pie3d';
 
 export class Details extends React.Component {
     constructor(props) {
         debugger
         super(props);
         this.state = {
-            itemSelected:{}
+            itemSelected:{},
+            dataForGraph:[]
         }
         this.componentWillMount=this.componentWillMount.bind(this);
     };
     componentWillMount = () =>{
+        var donesCount=this.props.data.filter(obj=>obj.IsDone==true).length;
+        var TodosCount=this.props.data.length-donesCount;
+        debugger
+        var data = [
+            {
+              value: donesCount,
+              color: '#f0ad4e',
+              label: 'Done'
+            },
+            {
+              value: TodosCount,
+              color: '#af4340',
+              label: 'Todo'
+            }
+          ];
         var s=this.props.match.params.id;    
         var found = this.props.data.find(function(element) {
             return element.id ==s;
@@ -22,6 +40,7 @@ export class Details extends React.Component {
           found.startDate=found.startDate==""?"There Is No startDate To Display.":found.startDate;
           found.endDate=found.endDate==""?"There Is No endDate To Display.":found.endDate;          
         this.setState({itemSelected:found});
+        this.setState({dataForGraph:data});
     }
     returnToList = (e) => {
         e.preventDefault();
@@ -29,9 +48,11 @@ export class Details extends React.Component {
     }
 
     render() {       
-        return (
+        return ( 
+            <div className="container"> 
+            <div className="pie-div"><Pie3D data={this.state.dataForGraph}/></div>            
             <div className="container details-container">
-            <div className="row details-header"><h1>Details Display Form</h1></div>
+                <div className="row details-header"><h1>Details Display Form</h1></div>
                 <div className="row">
                     <div className='col col-md-2' ><h2>Id:</h2></div>
                     <div className='col col-md-2'><h2>{this.state.itemSelected.id}</h2></div>
@@ -57,6 +78,7 @@ export class Details extends React.Component {
                     <div className='col col-md-6' ><h2>{this.state.itemSelected.Remark}</h2></div>
                 </div>
             </div>
+            </div>  
         );
     }
 }
